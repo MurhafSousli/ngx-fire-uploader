bc1609424135d97fb453c45773bf23df2b8f4141
Murhaf Sousli <murhafsousli@gmail.com>
2018-05-24 09:36:47 +0300

    update build

--- .config/styles.js
+++ .config/styles.js
@@ -1,100 +1,82 @@
-const sass = require('node-sass');
-const fs = require('fs');
-const path = require('path');
+const replaceExt = require('replace-ext');
 const mkdirp = require('mkdirp');
-const cssnano = require('cssnano');
+const c = require('ansi-colors');
+const path = require('path');
+
+const {
+  existsSync,
+  writeFileSync,
+  readdirSync,
+  lstatSync,
+  createWriteStream,
+  createReadStream
+} = require('fs');
+
+const sass = require('node-sass');
 const postcss = require('postcss');
+
+// post css processors
+const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 const stripInlineComments = require('postcss-strip-inline-comments');
 
-const gulpUtil = require('gulp-util');
+const processors = [stripInlineComments, autoprefixer, cssnano];
 
-const processors = [
-  stripInlineComments,
-  autoprefixer,
-  cssnano
-];
-
-const CORE_SRC = 'lib/core/src/';
-const CORE_DIST = 'build/core/';
-
-const MANAGER_SRC = 'lib/manager/src/';
-const MANAGER_DIST = 'build/manager/';
-
-function writeStyleFile(file) {
-
-  console.log(file);
-  /** Create dir if not exists */
-  if (!fs.existsSync(path.dirname(file.path))) {
-    mkdirp.sync(path.dirname(file.path), function (err) {
-      if (err) gulpUtil.log('[mkdir]:', err)
-    });
-  }
-
-  /** Write css files to build dir */
-  fs.writeFileSync(file.path, file.contents, function (err) {
-    if (!err) {
-      gulpUtil.log('[writeFileSync]:', err);
-    }
-  });
-}
-
-
-function start(startPath, filter, callback) {
-
-  if (!fs.existsSync(startPath)) {
-    console.log("no dir ", startPath);
+function compileScss(startPath, filter, callback) {
+  if (!existsSync(startPath)) {
+    logError(startPath);
     return;
   }
 
-  const files = fs.readdirSync(startPath);
+  const files = readdirSync(startPath);
   for (var i = 0; i < files.length; i++) {
-    var filename = path.join(startPath, files[i]);
-    var stat = fs.lstatSync(filename);
-    if (stat.isDirectory()) {
-      start(filename, filter, callback); //recurse
+    const fileName = path.join(startPath, files[i]);
+
+    if (lstatSync(fileName).isDirectory()) {
+      start(fileName, filter, callback); //recurse
+    } else if (filter.test(fileName)) {
+      callback(fileName);
     }
-    else if (filter.test(filename)) callback(filename);
   }
 }
 
 function sassTask(srcDir, distDir, filePath) {
-
   const theme = path.relative(srcDir, filePath);
 
-  /** Compile scss to css */
-  const sassObj = sass.renderSync({
-    file: filePath
-  });
+  // Compile scss to css
+  const sassObj = sass.renderSync({ file: filePath });
 
   if (sassObj && sassObj.css) {
     const css = sassObj.css.toString('utf8');
-    postcss(processors).process(css).then(function (result) {
+    postcss(processors)
+      .process(css)
+      .then(result => {
+        // Log warnings if any exists
+        result.warnings().map(logError);
 
-      /** Log warnings if any exists */
-      result.warnings().forEach(function (warn) {
-        gulpUtil.log(warn.toString());
+        cssContent = new Buffer(result.css);
+        cssPath = replaceExt(distDir + theme, '.css');
+
+        // Create dist directory if doest not exist
+        if (!existsSync(path.dirname(filePath))) {
+          mkdirp.sync(path.dirname(filePath), logError);
+        }
+        // Write css file to dist
+        writeFileSync(cssPath, cssContent, logError);
+
+        // Copy scss file to dist
+        createReadStream(filePath).pipe(createWriteStream(distDir + theme));
+
+        console.log(c.bgMagenta(path.basename(cssPath)), c.green(c.symbols.check));
       });
-
-      /** Create css file object */
-      const file = {};
-      file.contents = new Buffer(result.css);
-
-      /** Write css files to button dir */
-      file.path = gulpUtil.replaceExtension(distDir + theme, '.css');
-
-      writeStyleFile(file);
-      fs.createReadStream(filePath).pipe(fs.createWriteStream(distDir + theme));
-    });
   }
 }
 
-// Compile core styles
-start(CORE_SRC, /\.scss$/, function (filename) {
-  sassTask(CORE_SRC, CORE_DIST, filename);
-});
+const logError = err => console.log(c.bgRedBright('[Error]:'), c.red(err));
 
-// Compile previewer styles
-start(MANAGER_SRC, /\.scss$/, function (filename) {
-  sassTask(MANAGER_SRC, MANAGER_DIST, filename);
-});
+const MANAGER_SRC = 'lib/manager/src/';
+const MANAGER_DIST = 'build/manager/';
+// Compile manager styles
+compileScss(MANAGER_SRC, /\.scss$/, fileName =>
+  sassTask(MANAGER_SRC, MANAGER_DIST, fileName)
+);
