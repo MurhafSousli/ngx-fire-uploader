d6fe9c7348cad483ae1d87ea5366305ba8dde764
Murhaf Sousli <murhafsousli@gmail.com>
2018-08-12 03:23:27 +0300

    Add forms support

--- projects/photo/src/lib/fire-photo.component.ts
+++ projects/photo/src/lib/fire-photo.component.ts
@@ -9,29 +9,40 @@ import {
   EventEmitter,
   TemplateRef,
   ElementRef,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  forwardRef
 } from '@angular/core';
 import { FileState, FireUploaderProgress, FileItem, FireUploaderRef, FireUploader, ResizeMethod } from '@ngx-fire-uploader/core';
 import { BehaviorSubject } from 'rxjs';
 import { tap, map, switchMap, take, filter } from 'rxjs/operators';
 import { FirePhoto } from './fire-photo.service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 export interface FirePhotoState {
   loading?: boolean;
   photo?: string;
   success?: boolean;
+  disabled?: boolean;
 }
+
+/** For form control usage */
+export const PHOTO_UPLOADER_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => FirePhotoComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'fire-photo',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./fire-photo.scss'],
-  templateUrl: './fire-photo.html'
+  templateUrl: './fire-photo.html',
+  providers: [PHOTO_UPLOADER_VALUE_ACCESSOR]
 })
-export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
+export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy, ControlValueAccessor {
 
   // Local state
-  readonly state$ = new BehaviorSubject<FirePhotoState>({loading: false});
+  readonly state$ = new BehaviorSubject<FirePhotoState>({loading: false, disabled: false});
 
   // Fire uploader ref
   uploaderRef: FireUploaderRef;
@@ -39,11 +50,12 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
   // Adds '.fire-photo-hover' class to '.fire-border'
   hover = false;
 
+  onChange: Function;
+
   // FirePhoto options
   @Input() id = 'root';
   @Input() loadingTemplate: TemplateRef<any>;
-  @Input() disabled: boolean;
-  @Input() src: string = this._manager.config.defaultImage;
+  @Input() value: string = this._manager.config.defaultImage;
 
   @Input() dropZone: boolean = this._manager.config.dropZone;
 
@@ -60,10 +72,14 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
   @Input() resizeHeight: number = this._manager.config.resizeHeight;
   @Input() resizeQuality: number = this._manager.config.resizeQuality;
 
+  @Input('disabled') set onDisabled(isDisabled: boolean) {
+    this.updateState({disabled: isDisabled});
+  }
+
   // UploaderRef events
   @Output() progress = new EventEmitter<FireUploaderProgress>();
   @Output() file = new EventEmitter<FileItem>();
-  @Output() value = new EventEmitter<string>();
+  @Output() valueChange = new EventEmitter<string>();
   @Output() complete = new EventEmitter();
   @Output() active = new EventEmitter<boolean>();
   @Output() error = new EventEmitter<any>();
@@ -71,16 +87,8 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
   constructor(private _uploader: FireUploader, private _manager: FirePhoto, private _el: ElementRef) {
   }
 
-  ngOnInit() {
-
-    // Auto-set thumb width and height
-    if (!this.thumbHeight && !this.thumbWidth) {
-      this.thumbWidth = this._el.nativeElement.clientWidth;
-      this.thumbHeight = this._el.nativeElement.clientHeight;
-    }
-
-    // Get uploader ref and set the config
-    this.uploaderRef = this._uploader.ref(this.id, {
+  private getConfig() {
+    return {
       multiple: false,
       accept: 'image/*',
       paramName: this.paramName,
@@ -94,7 +102,19 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
       resizeMethod: this.resizeMethod,
       resizeWidth: this.resizeWidth,
       resizeQuality: this.resizeQuality
-    });
+    };
+  }
+
+  ngOnInit() {
+
+    // Auto-set thumb width and height
+    if (!this.thumbHeight && !this.thumbWidth) {
+      this.thumbWidth = this._el.nativeElement.clientWidth;
+      this.thumbHeight = this._el.nativeElement.clientHeight;
+    }
+
+    // Get uploader ref and set the config
+    this.uploaderRef = this._uploader.ref(this.id, this.getConfig());
 
     // Get generated thumbnail
     this.uploaderRef.files$.pipe(
@@ -117,7 +137,11 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
 
     this.uploaderRef.value$.subscribe((downloadURLs: string[]) => {
       this.updateState({photo: downloadURLs[0], success: true});
-      this.value.next(downloadURLs[0]);
+      this.valueChange.next(downloadURLs[0]);
+      // If used as a form control, call onChange function
+      if (this.onChange) {
+        this.onChange(downloadURLs[0]);
+      }
     });
 
     this.uploaderRef.active$.subscribe((active: boolean) => {
@@ -153,31 +177,19 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['src'] && changes['src'].currentValue !== changes['src'].previousValue) {
-      this.updateState({photo: this.src});
+    if (changes['value'] && changes['value'].currentValue !== changes['value'].previousValue) {
+      this.updateState({photo: this.value});
     }
 
     if (this.uploaderRef instanceof FireUploaderRef) {
       // Update uploader's config when inputs change
-      this.uploaderRef.setConfig({
-        paramName: this.paramName,
-        paramDir: this.paramDir,
-        uniqueName: this.uniqueName,
-        autoStart: this.autoStart,
-        thumbWidth: this.thumbWidth,
-        thumbHeight: this.thumbHeight,
-        thumbMethod: this.thumbMethod,
-        resizeHeight: this.resizeHeight,
-        resizeMethod: this.resizeMethod,
-        resizeWidth: this.resizeWidth,
-        resizeQuality: this.resizeQuality
-      });
+      this.uploaderRef.setConfig(this.getConfig());
     }
   }
 
   // Open file dialog
   select() {
-    if (!this.disabled) {
+    if (!this.state$.value.disabled) {
       this.uploaderRef.select();
     }
   }
@@ -201,11 +213,32 @@ export class FirePhotoComponent implements OnInit, OnChanges, OnDestroy {
   reset() {
     this.uploaderRef.reset();
     if (!this.state$.value.success) {
-      this.updateState({photo: this.src});
+      this.updateState({photo: this.value});
     }
   }
 
   updateState(state: FirePhotoState) {
     this.state$.next({...this.state$.value, ...state});
+  }
+
+  /**
+   * Custom form control functions
+   */
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.updateState({disabled: isDisabled});
+  }
+
+  writeValue(defaultPhoto: string): void {
+    if (defaultPhoto) {
+      this.updateState({photo: defaultPhoto});
+    }
   }
 }
