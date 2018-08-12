db972c5f85c925a09ee75a87d587dc4e2a97ea71
Murhaf Sousli <murhafsousli@gmail.com>
2018-08-12 12:41:14 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyB1Tm6QS1rvTV0Q-dgjWEgb_fm3kSLa1OM',
-    authDomain: 'kelshi-cf1d9.firebaseapp.com',
-    databaseURL: 'https://kelshi-cf1d9.firebaseio.com',
-    projectId: 'kelshi-cf1d9',
-    storageBucket: 'kelshi-cf1d9.appspot.com',
-    messagingSenderId: '380135811040'
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    authDomain: 'x.firebaseapp.com',
+    databaseURL: 'https://x.firebaseio.com',
+    projectId: 'xxxxxx',
+    storageBucket: 'xxxxx.appspot.com',
+    messagingSenderId: 'xxxxxxxxxxx'
   }
 };
 
