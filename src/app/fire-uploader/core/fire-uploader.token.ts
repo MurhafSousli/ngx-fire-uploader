e97de56441e5b7bc75007742f2659f1f5e6ab513
Murhaf Sousli <murhafsousli@gmail.com>
2018-08-12 10:02:09 +0300

    remove lib from demo folder

--- src/app/fire-uploader/core/fire-uploader.token.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { FireUploaderConfig } from './fire-uploader.model';
-
-/** FireUploaderConfig token */
-export const UPLOADER_CONFIG = new InjectionToken<FireUploaderConfig>('config');
--- src/app/fire-uploader/photo/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './fire-photo.component';
-export * from './fire-photo.model';
-export * from './fire-photo.module';
-export * from './fire-photo';
